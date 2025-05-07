5fbe26b689e9ced0cde30d62814c382cc35b5b77
Nathan Brake <33383515+njbrake@users.noreply.github.com>
2025-05-07 12:26:34 -0400

    Simplify Evaluation Code (#220)
    * clean up and simplify eval code
    
    * downgrade a log statement to debug
    
    * lint

--- tests/docs/test_all.py
+++ tests/docs/test_all.py
@@ -4,22 +4,22 @@ from unittest.mock import AsyncMock, MagicMock, patch
 import pytest
 from mktestdocs import check_md_file
 
+from flow_portal.evaluation.evaluators.schemas import TraceEvaluationResult
+
 
 # Note the use of `str`, makes for pretty output
 @pytest.mark.parametrize("fpath", pathlib.Path("docs").glob("**/*.md"), ids=str)
 def test_files_all(fpath: pathlib.Path) -> None:
     mock_agent = MagicMock()
     mock_create = MagicMock(return_value=mock_agent)
+    mock_eval = MagicMock()
+    mock_eval.return_value = MagicMock(spec=TraceEvaluationResult)
 
     mock_create_async = AsyncMock()
     with (
         patch("builtins.open", new_callable=MagicMock),
-        patch(
-            "flow_portal.evaluation.evaluation_runner.save_evaluation_results",
-            return_value=None,
-        ),
-        patch("flow_portal.evaluation.evaluation_runner.EvaluationRunner.run"),
         patch("flow_portal.AnyAgent.create", mock_create),
+        patch("flow_portal.evaluation.evaluate", mock_eval),
         patch("flow_portal.AnyAgent.create_async", mock_create_async),
     ):
         check_md_file(fpath=fpath, memory=True)  # type: ignore[no-untyped-call]
